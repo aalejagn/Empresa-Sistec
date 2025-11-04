78cbb80e6288e5409d4cfcb36ee90d4ddee941b9
aalejagn <ag0013155@gmail.com>
2025-11-04 14:55:00 -0600

    Cambios en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Registrar from './pages/Registro';
-import Header from './components/Header';
-import Home from './pages/Homee';
-import Login from './pages/Loginn';
+import Home from './pages/home';
+import Login from './pages/login';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path='/registro' element= {<Registrar/>}/>
-      <Route path='/header' element = {<Header/>}/>
+      <Route path='/registro' element= {<Registrar />}/>
     </Routes>
   );
 };
