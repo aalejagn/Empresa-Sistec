c08accd088be719ba1be5c23f1fb78ff9b64b348
aalejagn <145643893+aalejagn@users.noreply.github.com>
2025-12-06 22:07:55 -0600

    PRUEBAS

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+
+// CSS
 import "./assets/css/global.css";
 import "./assets/css/catalago.css";
 import "./assets/css/contactanos.css";
 import "./assets/css/footer.css";
-import "./assets/css/global.css";
 import "./assets/css/header.css";
-import "./assets/css/legal-pages.css";
-// import './assets/css/login.css'
-// import './assets/css/registrar.css'
 import "./assets/css/auth.css";
 import "./assets/css/style.css";
 import "./assets/css/ubicacion.css";
 import "./assets/css/valores.css";
-import "./components/CartContext";
-import { CartProvider } from "./components/CartContext";
 import "./assets/css/carrito.css";
 import "./assets/css/checkout.css";
-import "./assets/css/perfil.css"
+import "./assets/css/perfil.css";
+
+// Providers (solo se importan los que se usan directamente)
+import { CartProvider } from "./components/CartContext.jsx";
 import { AuthProvider } from "./components/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
